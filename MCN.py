9de0f7bbf1f2bf35de0eceb7e9116af984804e9a
Mattias <mattias@kockum.net>
2021-02-22 18:19:02 +0100

    first commit

---
+++ MCN.py
@@ -0,0 +1,18 @@
+#!usr/bin/env python3
+
+"""
+Code written by Mattias Kockum
+On 22/02/2021
+The aim of this program is to
+create a multi cortex network
+"""
+
+import numpy as np
+
+
+def main():
+    return(0)
+
+
+if __name__ == "__main__":
+    main()
